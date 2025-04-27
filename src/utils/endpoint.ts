256fb855f4c2a595edc76b7243248d756af318e3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-27 14:58:32 +0200

    dev: 0.0.5

--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -2,7 +2,8 @@ export const getEndPoint = () => {
   const env = process.env;
   let endpoint = env.UPTODOC_ENDPOINT;
   if (!endpoint) {
-    endpoint = "https://github.com/jlg-formation/uptodoc";
+    endpoint =
+      "https://raw.githubusercontent.com/jlg-formation/uptodoc/master/docs";
   }
   return endpoint;
 };
